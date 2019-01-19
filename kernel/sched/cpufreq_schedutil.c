4990c4e292c2c824600768068bffabd951bdc9d6
Josh Choo <dev@joshuous.com>
2019-01-19 12:10:14 +0800

    schedutil: Don't assume we are likely using PELT
    This causes needless overheads if we are using WALT by default.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -175,8 +175,9 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
 
 	cfs_max = arch_scale_cpu_capacity(NULL, cpu);
 
-	*util = min(rq->cfs.avg.util_avg, cfs_max);
-	*max = cfs_max;
+	*util = boosted_cpu_util(cpu);
+	if (use_pelt())
+		*util = *util + rt;
 
 	*util = boosted_cpu_util(cpu, &loadcpu->walt_load);
 }
