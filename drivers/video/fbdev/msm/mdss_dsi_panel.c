b565ba481c32a2a48393d7dc9300b8e176995252
alanndz <alanmahmud0@gmail.com>
2019-04-14 15:41:04 +0000

    Revert "display: add a simple api to query the display state (on/off)"
    This reverts commit 7ec216e3d94f4a594b8f364ce7251f4423aa6bb6.

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -22,7 +22,6 @@
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
 #include <linux/string.h>
-#include <linux/display_state.h>
 
 #include "mdss_dsi.h"
 #include "mdss_debug.h"
@@ -47,14 +46,6 @@ int LCM_effect[3] = {0x2,0xf0,0xf00};
 #endif
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
-
-bool display_on = true;
-
-bool is_display_on()
-{
-	return display_on;
-}
-
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -949,8 +940,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
-	display_on = true;
-
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1207,8 +1196,6 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
 
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
-
-	display_on = false;
 
 end:
 	/* clear idle state */
